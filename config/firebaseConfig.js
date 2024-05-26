161d9a6cbc65bcf39128dd8c3ecf1e9220ddfb4b
Joshtri <stuffofyos1516@gmail.com>
2024-05-25 17:04:19 -0700

    fix: credential issue

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,19 +3,23 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 import { getAuth } from "firebase/auth";
+import dotenv from 'dotenv';
+// import { process } from 'node:process';
+
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAKViPpb-J-kj8Wzp8qkhf-9e89NEuLOwk",
-    authDomain: "sibercsms-limau.firebaseapp.com",
-    projectId: "sibercsms-limau",
-    storageBucket: "sibercsms-limau.appspot.com",
-    messagingSenderId: "548279737729",
-    appId: "1:548279737729:web:e6cb4e3817d97afc01dc80",
-    measurementId: "G-LFCQQBXPSG"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
